14440786e55c2723322fe0377b60618b771debd3
Scarlet Angelina Espinoza Moreno <scanespi@espol.edu.ec>
2020-06-26 02:51:02 -0500

    cambios

---
+++ sistemaTurnos/src/Modulo1/ventanaCrearPuesto.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modulo1;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author scarlet Espinoza
+ */
+public class ventanaCrearPuesto {
+    private StackPane root = new StackPane();
+    private Pane root2 = new Pane();
+    private ImageView fondo = new ImageView ("imagenes/fondoInicio.jpg");
+
+    public ventanaCrearPuesto() {
+        OrganizarVentana();
+    }
+     
+    public void OrganizarVentana(){
+          root.getChildren().addAll(fondo,root2);
+      }
+
+    public StackPane getRoot() {
+        return root;
+    }
+
+    public void setRoot(StackPane root) {
+        this.root = root;
+    }
+
+    public Pane getRoot2() {
+        return root2;
+    }
+
+    public void setRoot2(Pane root2) {
+        this.root2 = root2;
+    }
+
+    public ImageView getFondo() {
+        return fondo;
+    }
+
+    public void setFondo(ImageView fondo) {
+        this.fondo = fondo;
+    }
+    
+    
+    
+    
+}
--- sistemaTurnos/src/sistematurnos/SistemaTurnos.java
+++ sistemaTurnos/src/sistematurnos/SistemaTurnos.java
@@ -5,7 +5,10 @@
  */
 package sistematurnos;
 
+import Modulo1.ventanaAcceder;
+import Modulo1.ventanaCrearMedico;
 import Modulo1.ventanaMenu;
+import Modulo1.ventanaCrearPuesto;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.scene.Scene;
@@ -23,8 +26,19 @@ public class SistemaTurnos extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        ventanaMenu ventanaAdministrar= new ventanaMenu();
-        Scene scVentana1 = new Scene(ventanaAdministrar.getRoot(),600,600);
+        
+        ventanaMenu ventanaInicio= new ventanaMenu();
+        ventanaCrearPuesto ventanaPuesto= new ventanaCrearPuesto();
+        ventanaCrearMedico ventanaMedico= new ventanaCrearMedico();
+        ventanaAcceder ventanaAcceder = new ventanaAcceder();
+        
+        Scene scVentana1 = new Scene(ventanaInicio.getRoot(),600,600);
+        Scene scVentana2 = new Scene(ventanaPuesto.getRoot(),600,600);
+        Scene scVentana3 = new Scene(ventanaMedico.getRoot(),600,600);
+        //Scene scVentana4 = new Scene(ventanaAcceder.getRoot(), 610, 390);
+        
+        ventanaInicio.getBtnPuestos().setOnAction(e -> primaryStage.setScene(scVentana2));
+        ventanaInicio.getBtnMedico().setOnAction(e -> primaryStage.setScene(scVentana3));
         primaryStage.setScene(scVentana1);
         primaryStage.setTitle("Servicio de atencion medica");
         primaryStage.show();
